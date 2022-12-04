61583703db47936a35d7a4c29c728ef5c937fde9
Josh Ghiloni <ghiloni@gmail.com>
2022-12-03 22:59:47 -0500

    Initial pages checkin

--- pkg/assets/fs.go
+++
@@ -1,6 +0,0 @@
-package assets
-
-import "embed"
-
-//go:embed css/*.css css/*.ttf *.html js/*.js js/aoc.wasm
-var StaticHTML embed.FS
--- pkg/inputs/fs.go
+++
@@ -1,6 +0,0 @@
-package inputs
-
-import "embed"
-
-//go:embed day*.txt
-var Exercises embed.FS
